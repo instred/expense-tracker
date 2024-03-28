a181a6878798fbb5f777c1c89137f195899cf4d9
instred <jakub.bok23@gmail.com>
2024-03-28 23:33:01 +0100

    added react init

--- backend-flask/api.py
+++ backend-flask/api.py
@@ -1,5 +1,7 @@
 # import necessary packages
 import flask
+import mariadb
+import sys
 
 # create the flask app
 app = flask.Flask(__name__)
@@ -8,6 +10,27 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 def index():
    return 'Success!'
+
+config = {
+    'host': '213.227.68.191',
+    'port': 42069,
+    'user': 'root',
+    'password': 'NieWiem124',
+    'database': 'expense-tracker'
+}
+
+
+@app.route('/api', methods=['GET'])
+def index2():
+   try:
+      conn = mariadb.connect(
+         **config
+      )
+   except mariadb.Error as e:
+      print(f"Error connecting to MariaDB Platform: {e}")
+      sys.exit(1)
+   print(conn.response)
+
 
 # run the app
 app.run()
